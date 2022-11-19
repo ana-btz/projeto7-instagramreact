8e97eb440b92dacf576d5e61671c1c3f2a681db9
ana_btz <silva.anabtz@gmail.com>
2022-11-19 18:03:37 -0300

    criada a lógica dos dados dinâmicos e implementado o like com duble click na imagem

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,17 +1,75 @@
+import React, { useState } from "react"
+
 export default function Posts() {
     const posts = [
-        { imgUsuario: "assets/img/meowed.svg", usuario: "meowed", imgPost: "assets/img/gato-telefone.svg", imgSeguidor: "assets/img/respondeai.svg", seguidor: "respondeai" },
-        { imgUsuario: "assets/img/barked.svg", usuario: "barked", imgPost: "assets/img/dog.svg", imgSeguidor: "assets/img/adorable_animals.svg", seguidor: "adorable_animals" }
+        {
+            imgUsuario: "assets/img/meowed.svg",
+            usuario: "meowed",
+            imgPost: "assets/img/gato-telefone.svg",
+            imgSeguidor: "assets/img/respondeai.svg",
+            eguidor: "respondeai",
+            likes: 101523
+        },
+
+        {
+            imgUsuario: "assets/img/barked.svg",
+            usuario: "barked",
+            imgPost: "assets/img/dog.svg",
+            imgSeguidor: "assets/img/adorable_animals.svg",
+            seguidor: "adorable_animals",
+            likes: 298123
+        }
     ]
 
     return (
         <div class="posts">
-            {posts.map((p) => <Post imgUsuario={p.imgUsuario} usuario={p.usuario} imgPost={p.imgPost} imgSeguidor={p.imgSeguidor} seguidor={p.seguidor} />)}
+            {posts.map((p) =>
+                <Post
+                    imgUsuario={p.imgUsuario}
+                    usuario={p.usuario}
+                    imgPost={p.imgPost}
+                    imgSeguidor={p.imgSeguidor}
+                    seguidor={p.seguidor}
+                    likes={p.likes} />)}
         </div>
     )
 }
 
 function Post(props) {
+    const [corIcone, setCorIcone] = useState("")
+    const [contorno, setContorno] = useState("-outline")
+
+    function salvar() {
+        setCorIcone("salvo")
+        setContorno("")
+    }
+
+    const [corDoLike, setCorDoLike] = useState("")
+    const [contornoDoLike, setContornoDoLike] = useState("-outline")
+    const [likes, setLikes] = useState(props.likes)
+
+    function curtir() {
+        if (corDoLike === "") {
+            setCorDoLike("curtido")
+            setContornoDoLike("")
+            setLikes(likes + 1)
+        } else {
+            setCorDoLike("")
+            setContornoDoLike("-outline")
+            setLikes(likes - 1)
+        }
+    }
+
+    const [imgPost, setImgPost] = useState(props.imgPost)
+
+    function curtirPelaImg() {
+        if (corDoLike === "") {
+            setCorDoLike("curtido")
+            setContornoDoLike("")
+            setLikes(likes + 1)
+        }
+    }
+
     return (
         <div class="post" data-test="post">
             <div class="topo">
@@ -25,25 +83,29 @@ function Post(props) {
             </div>
 
             <div class="conteudo">
-                <img src={props.imgPost} data-test="post-image" />
+                <img src={imgPost} onDoubleClick={curtirPelaImg} data-test="post-image" />
             </div>
 
             <div class="fundo">
                 <div class="acoes">
                     <div>
-                        <ion-icon name="heart-outline" data-test="liske-post"></ion-icon>
+                        <ion-icon
+                            name={"heart" + contornoDoLike}
+                            class={corDoLike}
+                            onClick={curtir}
+                            data-test="liske-post"></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
                     <div>
-                        <ion-icon name="bookmark-outline" data-test="save-post"></ion-icon>
+                        <ion-icon name={"bookmark" + contorno} class={corIcone} onClick={salvar} data-test="save-post"></ion-icon>
                     </div>
                 </div>
 
                 <div class="curtidas">
                     <img src={props.imgSeguidor} />
                     <div class="texto">
-                        Curtido por <strong>{props.seguidor}</strong> e <strong data-test="like-number">outras 101.523 pessoas</strong>
+                        Curtido por <strong>{props.seguidor}</strong> e <strong data-test="like-number">outras {likes} pessoas</strong>
                     </div>
                 </div>
             </div>
--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,20 +1,31 @@
-export default function Usuario() {
+import React, {useState} from "react"
 
+export default function Usuario() {
     return (
-        <ElementoUsuario foto="assets/img/catanacomics.svg" nick="catanacomics" nome="Catana" />
+        <ElementoUsuario foto="assets/img/catanacomics.svg" nick="catanacomics" nome={"Catana"} />
     )
 }
 
 function ElementoUsuario(props) {
-    
+    const [nome, setNome] = useState(props.nome)
+    const [foto, setFoto] = useState(props.foto)
+
+    function alterarNome() {
+        setNome(prompt("Digite seu novo nome"))
+    }
+
+    function alterarFoto() {
+        setFoto(prompt("Por favor, insira o novo link"))
+    }
+
     return (
         <div class="usuario" data-test="user">
-            <img src={props.foto} data-test="profile-image" />
+            <img src={foto} onClick={alterarFoto} data-test="profile-image" />
             <div class="texto">
                 <strong>{props.nick}</strong>
                 <span data-test="name">
-                    {props.nome}
-                    <ion-icon name="pencil" data-test="edit-name"></ion-icon>
+                    {nome}
+                    <ion-icon name="pencil" onClick={alterarNome} data-test="edit-name"></ion-icon>
                 </span>
             </div>
         </div>
